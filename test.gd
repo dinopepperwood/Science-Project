b2e178ee7edec1e17ff265696b27084544273861
Bob <101481270+dinopepperwood@users.noreply.github.com>
2026-04-25 18:56:56 -0500

    Added Mass Label and changed keys to change mass from mouse wheel to Q and E

--- test.gd
+++ test.gd
@@ -5,10 +5,10 @@ func _ready():
 
 func _input(event: InputEvent) -> void:
 	if Global.current != null:
-		if(MOUSE_BUTTON_WHEEL_UP):
-			Global.current.mass += 1
-		elif(MOUSE_BUTTON_WHEEL_DOWN):
-			Global.current.mass -=1
+		if Input.is_action_just_pressed("Mass_Increase"):
+			Global.current.mass+=1
+		elif Input.is_action_just_pressed("Mass_Decrease"):
+			Global.current.mass-=1
 
 func _physics_process(delta: float) -> void:
 	pass
