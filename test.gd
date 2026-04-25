5e90ea982823923f9228a0f540b0a28ddd964dc0
Bob <101481270+dinopepperwood@users.noreply.github.com>
2026-04-25 18:58:22 -0500

    Fixed error of Mass being negative

--- test.gd
+++ test.gd
@@ -7,7 +7,7 @@ func _input(event: InputEvent) -> void:
 	if Global.current != null:
 		if Input.is_action_just_pressed("Mass_Increase"):
 			Global.current.mass+=1
-		elif Input.is_action_just_pressed("Mass_Decrease"):
+		elif Input.is_action_just_pressed("Mass_Decrease") and Global.current.mass>1:
 			Global.current.mass-=1
 
 func _physics_process(delta: float) -> void:
