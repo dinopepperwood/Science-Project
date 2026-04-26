b729b477f36865a793d79dc09cac846525049fcb
Bob <101481270+dinopepperwood@users.noreply.github.com>
2026-04-25 20:18:36 -0500

    Made it check every frame if Mass pressed

--- test.gd
+++ test.gd
@@ -5,9 +5,9 @@ func _ready():
 
 func _input(event: InputEvent) -> void:
 	if Global.current != null:
-		if Input.is_action_just_pressed("Mass_Increase"):
+		if Input.is_action_pressed("Mass_Increase"):
 			Global.current.mass+=1
-		elif Input.is_action_just_pressed("Mass_Decrease") and Global.current.mass>1:
+		elif Input.is_action_pressed("Mass_Decrease") and Global.current.mass>1:
 			Global.current.mass-=1
 		elif Input.is_action_pressed("Increase_Force"):
 			Global.current.force += 1
